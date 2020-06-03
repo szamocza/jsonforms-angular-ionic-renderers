af30045316e1ac1214f5f25c103f7e51fcd89bc5
jack <jack>
2020-06-03 14:43:30 +0200

    fixed typo

--- src/controls/boolean/boolean-checkbox-control.ts
+++ src/controls/boolean/boolean-checkbox-control.ts
@@ -40,7 +40,7 @@ import { JsonFormsControl } from 'jsonforms/packages/angular';
           <ion-icon [name]="filterOn ? 'ios-funnel' : 'ios-funnel-outline'"></ion-icon>
         </button>
         <img class="cb-picture" *ngIf="uischema && uischema.options && uischema.options.pictureUri" 
-             [src]="uischema.options.pictureUri" height="{{this.height}}" width="{{this.width}}" />
+             [src]="uischema.options.pictureUri" [height]="height" [width]="width" />
         <span>{{ label }}</span>
       </ion-label>
       <ion-label stacked *ngIf="error" color="danger">{{ error | translate }}</ion-label>
